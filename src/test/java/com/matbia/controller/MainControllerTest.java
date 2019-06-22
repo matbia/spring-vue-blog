b6cecfc9bc612949de133e2c6ab4dc7161dec35f
Mateusz Biały <mateusz.bialy@pm.me>
2019-06-22 12:53:04 +0200

    Updated tests

---
+++ src/test/java/com/matbia/controller/MainControllerTest.java
@@ -0,0 +1,26 @@
+package com.matbia.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(MainController.class)
+public class MainControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void showIndex() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("index"));
+    }
+}
